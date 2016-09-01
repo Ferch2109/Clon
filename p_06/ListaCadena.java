c2d6456148092e6f4e671e48cee849436bea0e64
Fernanda González <marifer.gusanito@gmail.com>
2016-08-31 22:35:41 -0500

    Se modificó ListaCadena.java de la practica 6

--- p_06/ListaCadena.java
+++ p_06/ListaCadena.java
@@ -1,44 +1,143 @@
+package mx.unam.ciencias.icc;
+
+import java.util.NoSuchElementException;
+
 /**
- * <p>Clase para listas doblemente ligadas de cadenas.</p>
+ * <p>Clase genérica para listas doblemente ligadas.</p>
  *
  * <p>Las listas nos permiten agregar elementos al inicio o final de la lista,
  * eliminar elementos de la lista, comprobar si un elemento está o no en la
  * lista, y otras operaciones básicas.</p>
  *
- * <p>Las listas tienen un iterador para poder recorrerlas.</p>
- *
- * @author Angel Ivan Gladin G
- * @version 1.0
- * @date 02-ago-15.
+ * <p>Las listas tienen un iterador para poder recorrerlas, y no aceptan a
+ * <code>null</code> como elemento.</p>
  */
-public class ListaCadena {
+public class Lista<T> {
 
-    /* Clase Nodo privada para uso interno de la clase ListaCadena. */
+    /* Clase Nodo privada para uso interno de la clase Lista. */
     private class Nodo {
-        public String elemento;
+        public T elemento;
         public Nodo anterior;
         public Nodo siguiente;
 
-        public Nodo(String elemento) {
-            this.elemento = elemento;
+        public Nodo(T elemento) {
+			this.elemento = elemento;
+            // Aquí va su código.
         }
     }
 
     /* Primer elemento de la lista. */
-    private Nodo cabeza = null;
+    private Nodo cabeza;
     /* Último elemento de la lista. */
-    private Nodo rabo = null;
+    private Nodo rabo;
     /* Nodo iterador. */
-    private Nodo iterador = null;
+    private Nodo iterador;
     /* Número de elementos en la lista. */
-    private int longitud = 0;
+    private int longitud;
 
+	/**
+     * Busca si existe un nodo con el parametro de entrada de tipo cadena.
+     * Regresa el nodo que contiene el elemento.
+     * @return el nodo en caso de que exista y <code>null</code> en caso de que no.
+     */
+    private Nodo buscaCadena( T elemento , Nodo n ){
+		if( n == null )
+			return null;
+		if( n.elemento.equals( elemento ) )
+			return n;
+		return buscaCadena( elemento , n.siguiente );
+	}
+	
+	/**
+	 * Elimina todos los nodos con elementos coincidentes al que se recibe como parametro.
+	 * @param el elemento a eliminar y el nodo en el que se busca.
+	 */
+	private void elimina( T elemento , Nodo n ){
+		if( n != null ){
+			if( n.elemento.equals( elemento ) ){
+				if( n.anterior == null )
+					eliminaPrimero();
+				else if( n.siguiente == null )
+					eliminaUltimo();
+				else{
+					n.siguiente.anterior = n.anterior;
+					n.anterior.siguiente = n.siguiente;
+					longitud--;
+				}
+				iterador = cabeza;
+			}
+			elimina( elemento , n.siguiente );
+		}
+	}
+	
+	/**
+	 * Regresa una nueva lista, en caso de que la direccion sea 1 la lista sera una copia
+	 * en caso de que la direccion sea 0 la lista sera una copia invertida.
+	 * @param Referencia a la nueva lista, nodo que contiene el elemento a agregar y direccion de la copia.
+	 * @return nueva lista
+	 */
+	private Lista<T> nuevaLista( Lista<T> l , Nodo n , int direccion ){
+		if( n == null )
+			return l;
+		l.agregaFinal( n.elemento );
+		return nuevaLista( l , ( ( direccion == 1 ) ? n.siguiente : n.anterior ) , direccion );
+	}
+	
+	/**
+	 * Obtiene el indice del nodo que contiene al elemento.
+	 * @param elemento el elemento del nodo del que se busca su indice,
+	 * nodo del que se comparara su elemento con el elemento parametro,
+	 * indice el contador de la posicion del nodo contenedor del elemento parametro.
+	 * @return indice del nodo que contiene el elemento parametro.
+	 */
+	private int indiceDe( T elemento , Nodo n , int indice ){
+		if( contiene( elemento ) )
+			if( !n.elemento.equals( elemento ) ){
+				indice++;
+				return indiceDe( elemento , n.siguiente , indice );
+			}
+			else
+				return indice;
+		return -1;
+	}
+	
+	/**
+	 * Regresa el elemento del nodo que esta en la posicion del indice i.
+	 * @param i, donde i es el indice del nodo que contiene el elemento a regresar
+	 * y n que es la referencia que apunta al objeto de tipo Nodo que se evalua.
+	 * @return el elemento del Nodo en la posicion indice i.
+	 * @throws ExcepcionIndiceInvalido si <em>i</em> es menor que cero o mayor o
+     *         igual que el número de elementos en la lista.
+	 */
+	private T get( int i , Nodo n ){
+		if( i < 0 || i >= longitud )
+			throw new ExcepcionIndiceInvalido();
+		if( i == 0 )
+			return n.elemento;
+		return get( i-1 , n.siguiente );
+	}
+	
+	/**
+     * Regresa una representación en cadena de la lista.
+     * @return una representación en cadena de la lista.
+     */
+    private String toString( String lista , Nodo n ){
+		if( n == null )
+			return lista+= String.format( "]" );
+		if( n != rabo ){
+			lista += String.format( "%s, " , n.elemento );
+			return toString( lista , n.siguiente );
+		}
+		return lista+= String.format( "%s]" , n.elemento );
+	}
+	
     /**
      * Regresa la longitud de la lista.
      * @return la longitud de la lista, el número de elementos que contiene.
      */
     public int getLongitud() {
-        return this.longitud;
+		return longitud;
+        // Aquí va su código.
     }
 
     /**
@@ -47,7 +146,8 @@ public class ListaCadena {
      *         otro caso.
      */
     public boolean esVacia() {
-        return this.longitud == 0;
+		return longitud == 0;
+        // Aquí va su código.
     }
 
     /**
@@ -55,24 +155,23 @@ public class ListaCadena {
      * el elemento a agregar será el primero y último. Después de llamar este
      * método, el iterador apunta a la cabeza de la lista.
      * @param elemento el elemento a agregar.
+     * @throws IllegalArgumentException si <code>elemento</code> es
+     *         <code>null</code>.
      */
-    public void agregaFinal(String elemento) {
-        Nodo rabo_anterior = null;
-        if (this.longitud > 0){
-            rabo_anterior = this.rabo;
-        }
-
-        this.rabo = new Nodo(elemento);
-        this.rabo.siguiente = null;
-        if (this.longitud < 1){
-            this.cabeza = this.rabo;
-            this.rabo.anterior = null;
-        } else {
-            this.rabo.anterior = rabo_anterior;
-            rabo_anterior.siguiente = this.rabo;
-        }
-        this.iterador = this.cabeza;
-        this.longitud += 1;
+    public void agregaFinal(T elemento) {
+		if( elemento == null ) 
+			throw new IllegalArgumentException();
+		Nodo ultimo = new Nodo( elemento );
+		if( longitud == 0 )
+			cabeza = rabo = ultimo;
+		else{
+			ultimo.anterior = rabo;
+			rabo.siguiente = ultimo;
+			rabo = ultimo;
+		}
+		longitud++;
+		iterador = cabeza;
+        // Aquí va su código.
     }
 
     /**
@@ -80,24 +179,25 @@ public class ListaCadena {
      * el elemento a agregar será el primero y último. Después de llamar este
      * método, el iterador apunta a la cabeza de la lista.
      * @param elemento el elemento a agregar.
+     * @throws IllegalArgumentException si <code>elemento</code> es
+     *         <code>null</code>.
      */
-    public void agregaInicio(String elemento) {
-        Nodo cabeza_anterior = null;
-        if (this.longitud > 0){
-            cabeza_anterior = this.cabeza;
-        }
-
-        this.cabeza = new Nodo(elemento);
-        this.cabeza.anterior = null;
-        if (this.longitud < 1){
-            this.rabo = this.cabeza;
-            this.cabeza.siguiente = null;
-        } else {
-            this.cabeza.siguiente = cabeza_anterior;
-            cabeza_anterior.anterior = this.cabeza;
-        }
-        this.iterador = this.cabeza;
-        this.longitud += 1;
+    public void agregaInicio(T elemento) {
+		if( elemento != null ){
+			Nodo primero = new Nodo( elemento );
+			if( longitud == 0 )
+				cabeza = rabo = primero;
+			else{
+				cabeza.anterior = primero;
+				primero.siguiente = cabeza;
+				cabeza = primero;
+			}
+			longitud++;
+			iterador = cabeza;
+		}
+		else
+			throw new IllegalArgumentException();
+        // Aquí va su código.
     }
 
     /**
@@ -106,68 +206,53 @@ public class ListaCadena {
      * modificado, el iterador se mueve al primer elemento.
      * @param elemento el elemento a eliminar.
      */
-    public void elimina(String elemento) {
-        this.primero();
-        eliminaAux(elemento);
-    }
-
-    private void eliminaAux(String elemento) {
-        if(!iteradorValido()) return;
-
-        if(iterador.elemento.equals(elemento)){
-            if(longitud == 1){
-                cabeza = null;
-                rabo = null;
-            } else if (iterador.equals(cabeza)) {
-                cabeza = cabeza.siguiente;
-                cabeza.anterior = null;
-            } else if (iterador.equals(rabo)){
-                rabo = rabo.anterior;
-                rabo.siguiente = null;
-            } else {
-                Nodo siguiente = iterador.siguiente;
-                iterador = iterador.anterior;
-                siguiente.anterior = iterador;
-                iterador.siguiente = siguiente;
-            }
-            longitud -= 1;
-            return;
-        }
-        siguiente();
-        eliminaAux(elemento);
+    public void elimina(T elemento) {
+		elimina( elemento , cabeza );
+        // Aquí va su código.
     }
 
     /**
      * Elimina el primer elemento de la lista y lo regresa.
-     * @return el primer elemento de la lista antes de eliminarlo, o
-     *         <code>null</code> si la lista está vacía.
+     * @return el primer elemento de la lista antes de eliminarlo.
+     * @throws NoSuchElementException si la lista es vacía.
      */
-    public String eliminaPrimero() {
-        if (this.cabeza != null) {
-            Nodo anterior = this.cabeza;
-            this.cabeza = this.cabeza.siguiente;   
-            this.cabeza.anterior = null;
-            this.longitud -= 1;
-            return anterior.elemento;
-        }
-        return null;
+    public T eliminaPrimero() {
+		if( cabeza != null ){
+			T e = cabeza.elemento;
+			if( cabeza != rabo ){
+				cabeza = cabeza.siguiente;
+				cabeza.anterior = null;
+			}
+			else
+				cabeza = rabo = null;
+			longitud--;
+			return e;
+		}
+		else
+			throw new NoSuchElementException();
+        // Aquí va su código.
     }
 
     /**
      * Elimina el último elemento de la lista y lo regresa.
-     * @return el último elemento de la lista antes de eliminarlo, o
-     *         <code>null</code> si la lista está vacía.
+     * @return el último elemento de la lista antes de eliminarlo.
+     * @throws NoSuchElementException si la lista es vacía.
      */
-    public String eliminaUltimo() {
+    public T eliminaUltimo() {
+		if( rabo != null ){
+			T e = rabo.elemento;
+			if( rabo.equals(cabeza) )
+				rabo = cabeza = null;
+			else{
+				rabo = rabo.anterior;
+				rabo.siguiente = null;
+			}
+			longitud--;
+			return e;
+		}
+		else
+			throw new NoSuchElementException();
         // Aquí va su código.
-        if (this.rabo != null) {
-            Nodo anterior = this.rabo;
-            this.rabo = this.rabo.anterior;   
-            this.rabo.siguiente = null;
-            this.longitud -= 1;
-            return anterior.elemento;
-        }
-        return null;
     }
 
     /**
@@ -176,40 +261,19 @@ public class ListaCadena {
      * @return <tt>true</tt> si <tt>elemento</tt> está en la lista,
      *         <tt>false</tt> en otro caso.
      */
-    public boolean contiene(String elemento){
-        primero();
-        return contieneAux(elemento);
-    }
-
-    private boolean contieneAux(String elemento) {
-        if(esVacia()) return false;
-        siguiente();
-        if(!iteradorValido()) return false;
-
-        if(iterador.elemento.equals(elemento)){
-            return true;
-        }
-        return contieneAux(elemento);
-          
+    public boolean contiene(T elemento) {
+		return buscaCadena( elemento , cabeza ) != null;
+        // Aquí va su código.
     }
 
     /**
      * Regresa la reversa de la lista.
      * @return una nueva lista que es la reversa la que manda llamar el método.
      */
-    public ListaCadena reversa() {
-        if (esVacia()) return new ListaCadena();
-        ultimo();
-        
-        return reversaAux(new ListaCadena());
-    }
-
-    private ListaCadena reversaAux(ListaCadena reversa){
-        if(!this.iteradorValido()) return reversa;
-        reversa.agregaFinal(iterador.elemento);
-        anterior();
-
-        return reversaAux(reversa);
+    public Lista<T> reversa() {
+		Lista<T> l = new Lista<T>();
+		return nuevaLista( l , rabo , 0 );
+        // Aquí va su código.
     }
 
     /**
@@ -217,17 +281,10 @@ public class ListaCadena {
      * lista que manda llamar el método, en el mismo orden.
      * @return una copiad de la lista.
      */
-    public ListaCadena copia() {
-        if (esVacia()) return new ListaCadena();
-        this.primero();
-        return copiaAux(new ListaCadena());
-    }
-
-    private ListaCadena copiaAux(ListaCadena copia) {
-        if (!iteradorValido()) return copia;
-        copia.agregaFinal(iterador.elemento);
-        siguiente();
-        return copiaAux(copia);
+    public Lista<T> copia() {
+		Lista<T> l = new Lista<T>();
+		return nuevaLista( l , cabeza , 1 );
+        // Aquí va su código.
     }
 
     /**
@@ -235,130 +292,113 @@ public class ListaCadena {
      * eliminar todos los elementos de la lista.
      */
     public void limpia() {
-        cabeza = null;
-        iterador = null;
-        rabo = null;
+		cabeza = rabo = null;
+		longitud = 0;
+        // Aquí va su código.
     }
 
     /**
      * Regresa el primer elemento de la lista.
-     * @return el primer elemento de la lista, o <code>null</code> si la lista
-     *         es vacía.
+     * @return el primer elemento de la lista.
+     * @throws NoSuchElementException si la lista es vacía.
      */
-    public String getPrimero() {
-        if (this.longitud == 0) {
-            return null;
-        }
-        return this.cabeza.elemento;
+    public T getPrimero() {
+		if( cabeza != null )
+			return cabeza.elemento;
+		throw new NoSuchElementException();
+        // Aquí va su código.
     }
 
     /**
      * Regresa el último elemento de la lista.
-     * @return el primer elemento de la lista, o <code>null</code> si la lista
-     *         es vacía.
+     * @return el primer elemento de la lista.
+     * @throws NoSuchElementException si la lista es vacía.
      */
-    public String getUltimo() {
-        if (this.longitud == 0) {
-            return null;
-        }
-        return this.rabo.elemento;
+    public T getUltimo() {
+		if( rabo != null )
+			return rabo.elemento;
+		throw new NoSuchElementException();
+        // Aquí va su código.
     }
 
     /**
-     * Regresa el <em>i</em>-ésimo elemento de la lista. Si el índice es menor
-     * que cero o mayor o igual que el número de elementos de la lista, el
-     * método regresa <tt>null</tt>.
+     * Regresa el <em>i</em>-ésimo elemento de la lista.
      * @param i el índice del elemento que queremos.
-     * @return el <em>i</em>-ésimo elemento de la lista, si <em>i</em> es mayor
-     *         o igual que cero y menor que el número de elementos en la lista;
-     *         <tt>null</tt> en otro caso.
+     * @return el <em>i</em>-ésimo elemento de la lista.
+     * @throws ExcepcionIndiceInvalido si <em>i</em> es menor que cero o mayor o
+     *         igual que el número de elementos en la lista.
      */
-    public String get(int i) {
-        if(getLongitud() < 1) return null;
-        this.primero();
-        return getAux(i, 0);
+    public T get(int i) {
+		return get( i , cabeza );
+        // Aquí va su código.
     }
 
-    private String getAux(int i, int x) {
-        if(!iteradorValido()) return null;
-        if (x == i && iteradorValido()) return iterador.elemento;
-        siguiente();
-        x++;
-        return getAux(i, x);
-    }
-    
     /**
      * Regresa el índice del elemento recibido en la lista.
      * @param elemento el elemento del que se busca el índice.
-     * @return el índice del elemento recibido en la lista, o -1 si el elemento
-     *         no está contenido en la lista.
+     * @return el índice del elemento recibido en la lista, o -1 si
+     *         el elemento no está contenido en la lista.
      */
-    public int indiceDe(String elemento) {
-        if(getLongitud() < 1) return -1;
-        this.primero();
-        return indiceDeAux(elemento, 0);
-    }
-
-    
-    private int indiceDeAux(String elemento, int index) {
-        if(!iteradorValido()) return -1;
-        if(iterador.elemento.equals(elemento)) return index;
-        index++;
-        siguiente();
-        return indiceDeAux(elemento, index);
+    public int indiceDe(T elemento) {
+		return indiceDe( elemento , cabeza , 0 );
+        // Aquí va su código.
     }
 
     /**
      * Regresa una representación en cadena de la lista.
      * @return una representación en cadena de la lista.
      */
-    public String toString() {
-        this.primero();
-        return toStringAux(new String());
-    }
-
-    private String toStringAux(String cadena) {
-        if(!iteradorValido()) return cadena;
-        cadena += this.get() + " ";
-        this.siguiente();
-        return toStringAux(cadena);
+    @Override public String toString() {
+		return toString( "[" , cabeza );
+        // Aquí va su código.
     }
 
     /**
      * Mueve el iterador de la lista a su primer elemento.
      */
     public void primero() {
-        this.iterador = this.cabeza;
+		iterador = cabeza;
+        // Aquí va su código.
     }
 
     /**
      * Mueve el iterador de la lista a su último elemento.
      */
     public void ultimo() {
-        this.iterador = this.rabo;
+		iterador = rabo;
+        // Aquí va su código.
     }
 
     /**
      * Mueve el iterador al siguiente elemento.
      */
     public void siguiente() {
-        this.iterador = this.iterador.siguiente;
+		if( iterador == null )
+			throw new NoSuchElementException();
+		iterador = iterador.siguiente;
+        // Aquí va su código.
     }
 
     /**
      * Mueve el iterador al elemento anterior.
      */
     public void anterior() {
-        this.iterador = this.iterador.anterior;
+		if( iterador == null )
+			throw new NoSuchElementException();
+		iterador = iterador.anterior;
+        // Aquí va su código.
     }
 
     /**
      * Regresa el elemento al que el iterador apunta.
-     * @return el elemento al que el iterador apunta, o <tt>null</tt> si el
-     *         iterador es inválido.
+     * @return el elemento al que el iterador apunta.
+     * @throws NoSuchElementException si el iterador es inválido.
      */
-    public String get() {
-        return this.iterador.elemento;
+    public T get() {
+		if( iterador == null )
+			throw new NoSuchElementException();
+		return iterador.elemento;
+        // Aquí va su código.
     }
 
     /**
@@ -367,7 +407,34 @@ public class ListaCadena {
      *         caso.
      */
     public boolean iteradorValido() {
+		return iterador != null;
         // Aquí va su código.
-        return this.iterador != null;
+    }
+
+    /**
+     * Nos dice si la lista es igual al objeto recibido.
+     * @param o el objeto con el que hay que comparar.
+     * @return <tt>true</tt> si la lista es igual al objeto recibido;
+     *         <tt>false</tt> en otro caso.
+     */
+    @Override public boolean equals(Object o) {
+        if (o == null)
+            return false;
+        if (!(o instanceof Lista))
+            return false;
+        @SuppressWarnings("unchecked") Lista<T> lista = (Lista<T>)o;
+        
+		if( lista.longitud != longitud )
+			return false;
+		Nodo n = cabeza;
+		Nodo n2 = lista.cabeza;
+		while( n != null ){
+			if( !n.elemento.equals( n2.elemento ) )
+				return false;
+			n = n.siguiente;
+			n2 = n2.siguiente;
+		}
+		return true;
+        // Aquí va su código.
     }
 }
